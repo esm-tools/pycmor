4e5ad5129f681df0c4fa9609d59428ee02685c5b
PavanSiligam <pavan.siligam@gmail.com>
2025-09-11 23:41:44 +0200

    style: fix whitespace in test_pyfesom2.py to resolve flake8 W293

--- tests/externals/test_pyfesom2.py
+++ tests/externals/test_pyfesom2.py
@@ -10,11 +10,11 @@ import pycmor.core.rule
 def test_read_grid_from_rule(fesom_pi_mesh_config):
     config = fesom_pi_mesh_config
     mesh_path = config["inherit"]["mesh_path"]
-    
+
     # Skip test if the test data is not available
     if not os.path.exists(mesh_path):
         pytest.skip(f"Test data not found at {mesh_path}")
-    
+
     try:
         rule = pycmor.core.rule.Rule.from_dict(config["rules"][0])
         rule.mesh_path = config["inherit"]["mesh_path"]
